f33431e25d9b301cfac6528149029f574b0ed5e4
dokhala <dokhaclam@gmail.com>
2021-02-17 17:50:32 +0700

    sua css link

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -6,24 +6,24 @@
         <meta name="description" content="">
         <meta name="author" content="">
         @yield('title')
-        <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
-        <link href="{{ asset('css/font-awesome.min.css') }}" rel="stylesheet">
-        <link href="{{ asset('css/prettyPhoto.css') }}" rel="stylesheet">
-        <link href="{{ asset('css/price-range.css') }}" rel="stylesheet">
-        <link href="{{ asset('css/animate.css') }}" rel="stylesheet">
-        <link href="{{ asset('css/main.css') }}" rel="stylesheet">
+        <link href="//{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+        <link href="//{{ asset('css/font-awesome.min.css') }}" rel="stylesheet">
+        <link href="//{{ asset('css/prettyPhoto.css') }}" rel="stylesheet">
+        <link href="//{{ asset('css/price-range.css') }}" rel="stylesheet">
+        <link href="//{{ asset('css/animate.css') }}" rel="stylesheet">
+        <link href="//{{ asset('css/main.css') }}" rel="stylesheet">
         @yield('css')
     </head>
     <body>
             @include('frontend.components.header')
             @yield('content')
             @include('frontend.components.footer')
-    <script src="{{ asset('js/jquery.js') }}"></script>
-    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
-    <script src="{{ asset('js/jquery.scrollUp.min.js') }}"></script>
-    <script src="{{ asset('js/price-range.js') }}"></script>
-    <script src="{{ asset('js/jquery.prettyPhoto.js') }}"></script>
-    <script src="{{ asset('js/main.js') }}"></script>
+    <script src="//{{ asset('js/jquery.js') }}"></script>
+    <script src="//{{ asset('js/bootstrap.min.js') }}"></script>
+    <script src="//{{ asset('js/jquery.scrollUp.min.js') }}"></script>
+    <script src="//{{ asset('js/price-range.js') }}"></script>
+    <script src="//{{ asset('js/jquery.prettyPhoto.js') }}"></script>
+    <script src="//{{ asset('js/main.js') }}"></script>
     @yield('js')
     </body>
 </html>
