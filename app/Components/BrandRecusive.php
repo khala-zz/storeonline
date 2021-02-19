f624cbb506279974cc1f2cb101c93c14d54682ae
khala-zz <dokhaclam@gmail.com>
2021-02-20 00:46:31 +0700

    Update BrandRecusive.php

--- app/Components/BrandRecusive.php
+++ app/Components/BrandRecusive.php
@@ -11,11 +11,12 @@ class BrandRecusive{
 	public function brandRecusiveAdd($parentId = 0,$subMark = ''){
 		
 		$data = Brand::where('parent_id',$parentId) -> get();
-		dd($data);
+		
 		foreach($data as $dataItem){
 			$this ->html .= "<option value = '".$dataItem -> id."'>".$subMark.$dataItem -> name."</option>";
 			$this -> brandRecusiveAdd($dataItem -> id,$subMark. '--');
 		}
+		dd('test4444444');
 		return $this -> html;
 	}
 
