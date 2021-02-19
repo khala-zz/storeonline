978586a0dd2acdfd4ffbd0444c9c94e0576d6b67
khala-zz <dokhaclam@gmail.com>
2021-02-19 11:50:19 +0700

    Update AdminLoginController.php

--- app/Http/Controllers/AdminLoginController.php
+++ app/Http/Controllers/AdminLoginController.php
@@ -12,7 +12,7 @@ class AdminLoginController extends Controller
     {
     	if(Auth::check())
     	{
-    		dd('test');
+    		
             return redirect() -> to('admin/brands');
     	}
     	return view('admin.login');
@@ -26,7 +26,7 @@ class AdminLoginController extends Controller
        
     	if(Auth::attempt(['email' => $request -> email,'password' => $request -> password],$remember))
     	{
-    		dd('test222');
+    		
             return redirect() -> to('admin/brands'); 
     	}
     }
