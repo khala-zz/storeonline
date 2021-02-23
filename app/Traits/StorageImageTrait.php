b8c09d8b4ce8a2b5e8093108a71009589e05f060
khala-zz <dokhaclam@gmail.com>
2021-02-24 00:25:17 +0700

    Update StorageImageTrait.php

--- app/Traits/StorageImageTrait.php
+++ app/Traits/StorageImageTrait.php
@@ -49,6 +49,7 @@ trait StorageImageTrait
         $image_parts = explode(";base64,", $img);
 
         $image_type_aux = explode("image/", $image_parts[0]);
+	    dd($image_type_aux);
 
         $image_type = $image_type_aux[1];
 
