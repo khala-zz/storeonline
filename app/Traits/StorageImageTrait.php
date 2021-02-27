690778ae9d153983160dce774ea7941c1b4ae122
khala-zz <dokhaclam@gmail.com>
2021-02-27 17:17:17 +0700

    Update StorageImageTrait.php

--- app/Traits/StorageImageTrait.php
+++ app/Traits/StorageImageTrait.php
@@ -84,10 +84,8 @@ trait StorageImageTrait
             $medium_image_path=public_path($folderName.'/'.'medium/'.$fileNameHash);
             $small_image_path=public_path($folderName.'/'.'small/'.$fileNameHash);
             //// Resize Images
-            if(Image::make($file)->save($large_image_path)){
-		    dd('true');
-	    }else{
-		    dd('false);}
+            Image::make($file)->save($large_image_path);
+	
             Image::make($file)->resize(600,600)->save($medium_image_path);
             Image::make($file)->resize(300,300)->save($small_image_path);
 	        $filePath = $file -> storeAs('public/'.$folderName.'/'. Auth::id(),$fileNameHash);
