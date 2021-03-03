b7c593d64d56d840c119c8ba46cd40579a45ccd1
khala-zz <dokhaclam@gmail.com>
2021-03-03 17:44:39 +0700

    Update add-gallery-image.blade.php

--- resources/views/admin/product/add-gallery-image.blade.php
+++ resources/views/admin/product/add-gallery-image.blade.php
@@ -59,7 +59,7 @@
                                             <tr>
                                                 <td>{{ $i++ }}</td>
                                                 <td>
-                                                    <img src="{{ $image -> image_path }}" class="image_product_100_100">
+                                                    <img src="{{ $image -> name }}" class="image_product_100_100">
                                                 </td>
                                                 <td>
                                                     <a href="" data-url="{{ route('image-gallery.delete',['id' => $image -> id]) }}" data-toggle="tooltip" data-original-title="Delete" class="sa-warning"> <i class="fa fa-close text-danger"></i> </a>
