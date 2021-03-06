929e3494aed70c185406b1d7c6e6294902ed5066
khala-zz <dokhaclam@gmail.com>
2021-03-06 17:36:48 +0700

    Update main.js

--- public/frontend/js/addToCart/main.js
+++ public/frontend/js/addToCart/main.js
@@ -13,6 +13,7 @@
     	let product_masp = $("#product_masp_"+dataId).val();
     	let product_color = $("#product_color_"+dataId).val();
     	let product_price = $("#product_price_"+dataId).val();
+    let product_size = $("#product_size_"+dataId).val();
     
     alert(product_id);
  	 
@@ -21,7 +22,7 @@
  	 		$.ajax({
  	 			url: '/addToCartAjax',
  	 			type: 'get',
- 	 			data: {_token: CSRF_TOKEN,product_id: product_id,product_name: product_name,product_masp: product_masp,product_color: product_color,product_price: product_price},
+ 	 			data: {_token: CSRF_TOKEN,product_id: product_id,product_name: product_name,product_masp: product_masp,product_color: product_color,product_price: product_price,product_size: product_size},
  	 			success: function(response){
  	 				alert(response);
  	 			}
