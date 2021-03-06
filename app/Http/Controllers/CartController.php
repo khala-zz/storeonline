4f09a4b4f8044de4bd52d1a6b8a7514aefe77cc9
khala-zz <dokhaclam@gmail.com>
2021-03-06 17:43:20 +0700

    Update CartController.php

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -84,6 +84,7 @@ class CartController extends Controller
         $inputToCart['price'] = $request -> price;
         $inputToCart['size'] = $request -> size;
         $inputToCart['quantity'] = 1;
+        dd($inputToCart);
         Session::forget('discount_amount_price');
         Session::forget('coupon_code');
       
