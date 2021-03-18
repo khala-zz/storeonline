c32c695ecacd2a04f4b944a2d250d406f1ea962d
khala-zz <dokhaclam@gmail.com>
2021-03-19 00:50:41 +0700

    Update header.blade.php

--- resources/views/frontend/components/header.blade.php
+++ resources/views/frontend/components/header.blade.php
@@ -30,7 +30,7 @@
 				<div class="row">
 					<div class="col-md-4 clearfix">
 						<div class="logo pull-left">
-							<a href="index.html"><img src="{{ asset('images/home/logo.png') }}" alt="logo" /></a>
+							<a href="{{ route('home') }}"><img src="{{ asset('images/home/logo.png') }}" alt="logo" /></a>
 						</div>
 						
 					</div>
